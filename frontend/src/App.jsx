af49bc1e2e95dc8b299a4da7c8d18d979351142f
chickenONsteak <austinchenwd@gmail.com>
2025-08-24 01:00:45 +0800

    hover behaviour

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import { Navigate, Route, Routes } from "react-router";
 import Homepage from "./components/Homepage";
 import EventsListingPage from "./components/EventsListingPage";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Homepage />} />
         <Route path="/events/:location" element={<EventsListingPage />} />
+        <Route path="/profile-page" element={<ProfilePage />} />
         <Route path="/*" element={<Navigate to="/home" replace />} />
       </Routes>
     </div>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleProfile = () => {
+    navigate("/profile-page");
+  };
+
   return (
     <div className="container">
       <div className="row my-4">
-        <div className="col-md-2">brand logo img</div>
+        <button className="col-md-2">brand logo img</button>
         <div className="col-md-8"></div>
-        <div className="col-md-2">profile</div>
+        <button className="col-md-2" onClick={handleProfile}>
+          profile
+        </button>
       </div>
     </div>
   );
