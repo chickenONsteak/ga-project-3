f49c291392947f1d6100397751279f52e15c318a
nalapapaya <madeline_lim_sy@hotmail.com>
2025-08-23 13:25:43 +0800

    added error handler & middleware for user and admin

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,25 @@ app.use(limiter); //apply rate
 app.use(express.json()); //Parse json into body
 app.use(express.urlencoded({ extended: false })); //use simple objects only
 
+//error handler for invalid body req format
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) { //broken JSON body
+    console.error("JSON parsing error:", err.message);
+    return res
+      .status(400)
+      .json({ status: "error", msg: "invalid JSON format" });
+  } else if ( //broken form data
+    err instanceof SyntaxError &&
+    err.status === 400 &&
+    err.type === "entity.parse.failed"
+  ) {
+    console.error("URL-encoded parsing error:", err.message);
+    return res
+      .status(400)
+      .json({ status: 400, msg: "invalid form data format" });
+  }
+});
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,56 @@
+import jwt from "jsonwebtoken";
+
+export const auth = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    //not logged in
+    return res.status(400).json({ status: "Error", msg: "No token found." });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", ""); //trim header
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET); //verify sign with secret = correct claims
+      req.decoded = decoded; //attach for downstream handler
+      next();
+    } catch (e) {
+      //if decoded expired or mismatched
+      console.error(e.message);
+      return res.status(401).json({ status: "Error", msg: "Unauthorised" });
+    }
+  } else {
+    //header exists but empty token
+    console.error("Missing token");
+    return res.status(403).json({ status: "Error", msg: "Missing token" });
+  }
+};
+
+export const authAdmin = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    //not logged in
+    return res.status(400).json({ status: "Error", msg: "No token found." });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", ""); //trim header
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET); //verify sign with secret = correct claims
+      if (decoded.role.toLowerCase() === "admin") {
+        //match role
+        req.decoded = decoded; //attach for downstream handler
+        next();
+      } else {
+        //wrong role
+        console.error("Unauthorised");
+        return res.status(403).json({ status: "Error", msg: "Unauthorised" });
+      }
+    } catch (e) {
+      //if expired or mismatch
+      console.error(e.message);
+      return res.status(401).json({ status: "Error", msg: "Unauthorised" });
+    }
+  } else {
+    //with header, no token
+    console.error("Missing token");
+    return res.status(403).json({ status: "Error", msg: "Missing token" });
+  }
+};
