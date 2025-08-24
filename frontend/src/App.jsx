b54d7c0807df2c85ab7eb38045879011768d1c6c
chickenONsteak <austinchenwd@gmail.com>
2025-08-24 22:01:57 +0800

    fix display login modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Navigate, Route, Routes } from "react-router";
 import Homepage from "./components/Homepage";
 import EventsListingPage from "./components/EventsListingPage";
 import ProfilePage from "./components/ProfilePage";
+import UserContext from "./context/user";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [role, setRole] = useState("");
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
   return (
     <div className="container">
       <NavBar />
-      <Routes>
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/events/:location" element={<EventsListingPage />} />
-        <Route path="/profile-page" element={<ProfilePage />} />
-        <Route path="/*" element={<Navigate to="/home" replace />} />
-      </Routes>
+      <UserContext.Provider
+        value={{ accessToken, setAccessToken, role, setRole }}
+      >
+        <Routes>
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/events/:location" element={<EventsListingPage />} />
+          <Route
+            path="/profile-page"
+            element={
+              <ProtectedRoute setShowLoginModal={setShowLoginModal}>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
