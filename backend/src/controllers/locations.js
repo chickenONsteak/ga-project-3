5ea9c57c1e5ab000ff248f407d9034f68493a47b
nalapapaya <madeline_lim_sy@hotmail.com>
2025-08-26 13:44:47 +0800

    added location controller for GET

--- backend/src/controllers/locations.js
+++ backend/src/controllers/locations.js
@@ -35,14 +35,16 @@ export const addLocation = async (req, res) => {
 
     // validate region
     const allowedRegions = ["north", "south", "east", "west"];
-    if (region && !allowedRegions.includes(region)) { //only accept above options
+    if (region && !allowedRegions.includes(region)) {
+      //only accept above options
       return res
         .status(400)
         .json({ status: "error", msg: "Invalid region value." });
     }
 
     // validate capacity
-    if (capacity !== null && (isNaN(capacity) || Number(capacity) < 1)) { // if not null check> Nan/ 0 = reject
+    if (capacity !== null && (isNaN(capacity) || Number(capacity) < 1)) {
+      // if not null check> Nan/ 0 = reject
       return res
         .status(400)
         .json({ status: "error", msg: "Capacity must be a positive number." });
@@ -65,3 +67,103 @@ export const addLocation = async (req, res) => {
       .json({ status: "error", msg: "Error creating location." });
   }
 };
+
+export const viewAllLocations = async (req, res) => {
+  try {
+    const locations = await LocationsModel.find();
+    const outputArray = [];
+    for (const location of locations) {
+      outputArray.push({
+        _id: location._id,
+        name: location.name,
+        address: location.address,
+        region: location.region,
+        capacity: location.capacity,
+      });
+    }
+    res.json(outputArray);
+  } catch (e) {
+    console.error(e.message);
+    return res
+      .status(500)
+      .json({ status: "error", msg: "Unable to fetch locations" });
+  }
+};
+
+export const getLocationById = async (req, res) => {
+  try {
+    const { locationId } = req.params; //get id from route
+    if (!mongoose.Types.ObjectId.isValid(locationId)) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Invalid locationId." });
+    }
+    const location = await LocationsModel.findById(locationId);
+
+    if (!location) {
+      return res
+        .status(404)
+        .json({ status: "error", msg: "Location not found." });
+    }
+
+    return res.json({ status: "ok", location });
+  } catch (e) {
+    console.error(e.message);
+    return res
+      .status(500)
+      .json({ status: "error", msg: "Unable to fetch location" });
+  }
+};
+
+export const removeLocation = async (req, res) => {
+  try {
+    // get logged in username
+    const username = (req.user?.username || req.decoded?.username || "")
+      .toLowerCase()
+      .trim();
+    if (!username) {
+      return res.status(401).json({ status: "error", msg: "Unauthorised" });
+    }
+
+    // check user exists
+    const user = await AuthModel.findOne({ username }).select("role _id");
+    if (!user) {
+      return res.status(401).json({ status: "error", msg: "Unauthorised" });
+    }
+
+    // ensure admin only access
+    if (user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ status: "error", msg: "Only admins can create locations." });
+    }
+
+    const { locationId } = req.params; //get id from route
+    const isValidId = (x) => mongoose.Types.ObjectId.isValid(x);
+    if (!isValidId(locationId)) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Invalid location id" });
+    }
+
+    const deleted = await LocationsModel.findOneAndDelete({
+      _id: locationId,
+    });
+
+    if (!deleted) {
+      return res.status(404).json({
+        status: "error",
+        msg: "Location not found or you are not the admin.",
+      });
+    }
+
+    return res.json({ status: "ok", msg: "Location deleted successfully." });
+  } catch (e) {
+    console.error(e.message);
+    return res.status(500).json({
+      status: "error",
+      msg: "Failed to delete location",
+    });
+  }
+};
+
--- backend/src/routers/locations.js
+++ backend/src/routers/locations.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { addLocation } from "../controllers/locations.js";
-import { authAdmin } from "../middleware/auth.js";
+import {
+  addLocation,
+  getLocationById,
+  removeLocation,
+  viewAllLocations,
+} from "../controllers/locations.js";
+import { auth, authAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
+//accessible to public
+router.get("/", viewAllLocations);
+router.get("/:locationId", getLocationById);
+
+router.use(auth);
+
 router.put("/", authAdmin, addLocation);
+router.delete("/:locationId", authAdmin, removeLocation)
 
 export default router;
